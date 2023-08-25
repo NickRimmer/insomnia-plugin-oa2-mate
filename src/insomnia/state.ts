c6a7a3d01bceaf2af2a7f586e7ea55640d7d9b35
Loïc Damet <loic.damet.e@thalesdigital.io>
2023-08-25 15:46:54 +0200

    Quickfix: fix 'getState' error after insomnia update

--- src/insomnia/state.ts
+++ src/insomnia/state.ts
@@ -1,37 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { BaseDoc } from './insomnia.types'
 
-let store: any = null
+let router: any = null
 
-export const getStore = (): any => {
-  if (!store) {
-    const root = document.querySelector('#root') as Record<string, any>
-    const parameter = Object.getOwnPropertyNames(root).findLast(x => x.startsWith('__reactContainer')) as string
-    store = root[parameter].memoizedState.element.props.store
+var getRouter = (): any => {
+  if (!router) {
+    const root = document.querySelector("#root") as Record<string, any>;
+    if (!root) {
+      return;
+    }
+    const parameter = Object.getOwnPropertyNames(root).findLast((x) => x.startsWith("__reactContainer"));
+    router = root[parameter as string]?.memoizedState?.element?.props?.router;
   }
+  return router;
+};
 
-  return store
+export const getState = () => {
+  const router = getRouter();
+  return router?.state;
+};
+
+export const getActiveWorkspaceId = (): string => {
+  const state = getState();
+  return state?.loaderData[":workspaceId"].activeWorkspace._id
 }
-
-export const getState = (): any => {
-  const store = getStore()
-  return store.getState()
-}
-
-export const getActiveWorkspace = (): BaseDoc => {
-  const state = getState()
-  const workspaceId = state.global.activeWorkspaceId
-  const workspace = state.entities.workspaces[workspaceId]
-  return workspace
-}
-
-export const getActiveEnvironment = (): BaseDoc | null => {
-  const state = getState()
-  const activeWorkspaceId = getActiveWorkspace()._id
-  const activeWorkspaceMeta = (Object.values(state.entities.workspaceMetas) as any[]).find(x => x.parentId === activeWorkspaceId) as BaseDoc
-  const activeEnvironmentId = activeWorkspaceMeta.activeEnvironmentId
-  const activeEnvironment = state.entities.environments[activeEnvironmentId] ?? null as (BaseDoc | null)
-
-  return activeEnvironment
+export const getActiveEnvironmentId = (): string => {
+  const state = getState();
+  return state?.loaderData[":workspaceId"].activeEnvironment._id
 }
